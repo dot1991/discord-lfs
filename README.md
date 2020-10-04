20fe05d1f97bace91b5a2065b22319364218feda
Lamp <ledlamp89@gmail.com>
2020-10-03 23:17:55 -0500

    Simplify; allow download without token

--- README.md
+++ README.md
@@ -1,31 +1,34 @@
 # Discord LFS
-A utility that uploads a file as large as you want, to Discord. It works by splitting the file into 8MB chunks and uploading them as attachments to separate channels on a guild so it's not affected by rate limits. The information of each attachment is collected and uploaded as a compressed file into one meta channel so you just keep that message id.
+A utility that uploads a file as large as you want, to Discord. It works by splitting the file into 8MB chunks and uploading them as attachments to separate channels on a guild so it's not affected by rate limits. The information of each attachment is collected and saved to a compressed file which anyone can use with this program to download the original file.
 
 ### Warning
 THIS IS API ABUSE! You may get in trouble if you upload ridiculously large amounts of data.
 
 ## Setup
-Node.js 12 or newer is required.
+Node.js 12.9 or newer is required.
 ```sh
 wget https://github.com/ledlamp/discord-lfs/archive/master.zip -O discord-lfs.zip
 unzip discord-lfs.zip
 cd discord-lfs
 npm ci
+chmod a+x dlfs.js
 ```
-Create a Discord bot and copy its token. Create a file named `.env`, type `TOKEN=` and paste the token after it.
+Unless you only need to download, create a Discord bot and copy its token. Create a file named `.env`, type `TOKEN=` and paste the token after it.
 
 ## Usage
 Open terminal in the folder.
 ```
 ./dlfs.js upload <path-to-file>
 ```
-You will receive a code when it's finished. You will need to use this code with the same bot account to download the file.
+When this is finished a .dlfs.gz file will be saved in the working directory. Anyone can use that metadata file with this program to download the original file.
 ```
-./dlfs.js download <code>
+./dlfs.js download <path or http url of .dlfs.gz file>
 ```
-This will download to the working directory, appending _ if the destination file already exists.
+This will download the original file, represented by the metadata (.dlfs.gz) file, into the working directory. This does not require a bot token.
 
 If you want to delete the file from Discord's servers:
 ```
-./dlfs.js delete <code>
+./dlfs.js delete <path or http url of .dlfs.gz file>
 ```
+
+(On Windows you may have to use `node dlfs.js` instead of `./dlfs.js`.)
--- dlfs.js
+++ dlfs.js
@@ -15,24 +15,22 @@ switch (command) {
 			else console.log(`wtf is dis\ngive me regular file!`);
 			process.exit(1);
 		}
-		var mode = 1;
+		uploadFile(input);
 		break;
 	case "download":
 		if (!input) { console.error(`download what bruh`); process.exit(1); }
-		if (isNaN(input)) { console.error(`input is not a number`); process.exit(1); }
-		var mode = 2;
+		downloadFile(input);
 		break;
 	case "delete":
 		if (!input) { console.error(`bruh`); process.exit(1); }
-		if (isNaN(input)) { console.error(`input is not a number`); process.exit(1); }
-		var mode = 3;
+		deleteFile(input);
 		break;
 	case undefined:
 		console.log(
 			`Discord Large-File-Storing Utility Thing\n` +
-			`To upload a file: ${process.argv.slice(0,2).join(' ')} upload <path>\n` +
-			`To download a file: ${process.argv.slice(0,2).join(' ')} download <code>\n` +
-			`To delete a file you uploaded: ${process.argv.slice(0,2).join(' ')} delete <code>`
+			`To upload a file: ${process.argv.slice(0,2).join(' ')} upload <path to any file>\n` +
+			`To download a file: ${process.argv.slice(0,2).join(' ')} download <http url or path to .dlfs.gz file>\n` +
+			`To delete a file you uploaded: ${process.argv.slice(0,2).join(' ')} <http url or path to .dlfs.gz file>`
 		);
 		process.exit();
 		break;
@@ -40,42 +38,37 @@ switch (command) {
 		console.log("bruh");
 		process.exit(1);
 }
-require("dotenv").config();
-if (!process.env.TOKEN) { console.error(`TOKEN environment variable not set. Set it with a valid bot token or paste it in the .env file.`);  }
 
 const DATA_GUILD_NAME = "datastore";
 const DATA_CHANNEL_NAME = "data";
-const META_CHANNEL_NAME = "meta";
 const CHUNK_SIZE = 8e6;
 
-var Discord = require("discord.js");
-var fetch = require("node-fetch");
-var fs = require("fs");
-var path = require("path");
-var zlib = require("zlib");
-
-var client = new Discord.Client({restRequestTimeout: 6e4});
-console.log("Logging in to Discord…");
-client.login(process.env.TOKEN).catch(error => {
-	console.error(`Login failed: ${error.message}\nMake sure to set the TOKEN environment variable to a valid bot token.`);
-	process.exit(1);
-});
-client.once("ready", async () => {
-	console.log(`Logged in as ${client.user.tag}`);
-	//console.debug("Guilds:", client.guilds.cache.map(g => g.name));
-	if (mode === 1) await uploadFile(input); else
-	if (mode === 2) await downloadFile(input); else
-	if (mode === 3) await deleteFile(input);
-	else console.error("wtf");
-	client.destroy();
-});
+async function prepareClient() {
+	require("dotenv").config();
+	var client = new (require("discord.js")).Client({restRequestTimeout: 6e4});
+	console.log("Logging in to Discord…");
+	await client.login(process.env.TOKEN).catch(error => {
+		console.error(`Login failed: ${error.message}\nMake sure to set the TOKEN environment variable to a valid bot token.`);
+		process.exit(1);
+	});
+	await new Promise(r => client.on("ready", r));
+	client.dataguild = client.guilds.cache.find(x => x.name == DATA_GUILD_NAME && x.ownerID == client.user.id);
+	if (!client.dataguild) {
+		console.log("Creating new data guild…");
+		client.dataguild = await client.guilds.create(DATA_GUILD_NAME, {
+			channels: [Array(500).fill({name: DATA_CHANNEL_NAME})]
+		});
+	} else console.log("Found data guild");
+	//console.debug(`ID: ${dg.id} Invite: ${(await dg.channels.cache.first().createInvite()).code}`);
+	return client;
+}
 
 async function uploadFile(filepath) {
-	var filename = path.basename(filepath);
-	var dataguild = await getDataGuild(client);
-	var datachannels = dataguild.channels.cache.filter(c => c.name == DATA_CHANNEL_NAME).array();
-	var attachment_list = [];
-	var readstream = fs.createReadStream(filepath);
+	var readstream = require("fs").createReadStream(filepath);
+	var client = await prepareClient();
+	var datachannels = client.dataguild.channels.cache.filter(c => c.name == DATA_CHANNEL_NAME).array();
+	var filename = require("path").basename(filepath);
+	var metadata = [JSON.stringify({filename})]; // still not sure what to call this
 	var index = 0;
 	while (true) {
 		let choncc = readstream.read(CHUNK_SIZE);
@@ -90,84 +83,54 @@ async function uploadFile(filepath) {
 		let message;
 		while (true) {
 			try {
-				message = await datachannels[index].send(new Discord.MessageAttachment(choncc, `${filename}.${index}`));
+				message = await datachannels[index].send(new (require("discord.js")).MessageAttachment(choncc, `${filename}.${index}`));
 				break;
 			} catch(error) {
 				console.error("bruh:", error.message, "retrying…");
 			}
 		}
 		let attachment = message.attachments.first();
-		attachment_list.push(`${message.channel.id},${message.id},${attachment.id},${attachment.name}`);
+		metadata.push(`${message.channel.id},${message.id},${attachment.id},${attachment.name}`);
 		index++;
 	}
-	console.log("All chunks uploaded; compressing attachment list…");
-	attachment_list = attachment_list.join(';');
-	attachment_list = zlib.gzipSync(attachment_list);
-	console.log("Uploading attachment list…");
-	const INDEXFILE_NAME = `${filename}.dlfs.gz`;
-	try {
-		let message = await dataguild.channels.cache.find(c => c.name == META_CHANNEL_NAME).send(new Discord.MessageAttachment(attachment_list, INDEXFILE_NAME));
-		var code = message.id;
-		console.log(`Done; save this code to download the file: ${code}`);
-		return code;
-	} catch(error) {
-		console.error(error.message);
-		fs.writeFileSync(attachment_list, path.join(path.dirname(filepath), INDEXFILE_NAME));
-		console.log(`The attachment list has been saved to ${INDEXFILE_NAME} instead.`);
-		return INDEXFILE_NAME;
-	}
+	console.log("All chunks uploaded; saving metadata…");
+	metadata = metadata.join(';');
+	metadata = require("zlib").gzipSync(metadata);
+	var metadatafilename = `${filename}.dlfs.gz`;
+	while (require("fs").existsSync(metadatafilename)) metadatafilename += '_';
+	require("fs").writeFileSync(metadatafilename, metadata);
+	console.log(`Metadata file written to ${metadatafilename}. Anyone with this metadata file can download the original file using this program.`);
+	client.destroy();
 }
 
-async function downloadFile(code) {
-	var dataguild = await getDataGuild(client);
-	var metachannel = dataguild.channels.cache.find(x => x.name == META_CHANNEL_NAME);
-	//console.debug(`Found meta channel ${metachannel.id}`);
-	console.log("Downloading index file…");
-	var message = await metachannel.messages.fetch(code);
-	var attachment_list = await (await fetch(message.attachments.first().url)).buffer();
-	console.log("Extracting index file…");
-	attachment_list = zlib.gunzipSync(attachment_list).toString();
-	attachment_list = attachment_list.split(';').map(x => x.split(','));
-	attachment_list.sort((a,b) => {
-		let ai = Number(a[3].split('.').pop());
-		let bi = Number(b[3].split('.').pop());
-		return ai - bi;
-	});
-	var filename = attachment_list[0][3].slice(0, -2);
-	while (fs.existsSync(filename)) filename += '_';
-	var writestream = fs.createWriteStream(filename);
+async function downloadFile(x) {
+	var {metadata, filename} = await loadMetadata(x);
+	while (require("fs").existsSync(filename)) filename += '_';
+	var writestream = require("fs").createWriteStream(filename);
 	var index = 0;
 	while (true) {
-		let dat = attachment_list[index];
+		let dat = metadata[index];
 		if (!dat) break;
 		let channel_id = dat[0], attachment_id = dat[2], attachment_name = dat[3];
 		let attachment_url = `https://cdn.discordapp.com/attachments/${channel_id}/${attachment_id}/${attachment_name}`;
 		console.log(`Downloading chunk #${index}…`);
-		let data = (await fetch(attachment_url)).body;
+		let data = (await require("node-fetch")(attachment_url)).body;
 		data.pipe(writestream, {end: false});
 		await new Promise(resolve => data.on("end", resolve));
 		index++;
 	}
 	writestream.end();
-	console.log(`Done! Downloaded file "${filename}"`);
+	console.log(`Done! Downloaded to "${filename}"`);
 }
 
-async function deleteFile(code) {
-	var dataguild = await getDataGuild(client);
-	var metachannel = dataguild.channels.cache.find(x => x.name == META_CHANNEL_NAME);
-	//console.debug(`Found meta channel ${metachannel.id}`);
-	console.log("Downloading index file…");
-	var message = await metachannel.messages.fetch(code);
-	var attachment_list = await (await fetch(message.attachments.first().url)).buffer();
-	console.log("Extracting index file…");
-	attachment_list = zlib.gunzipSync(attachment_list).toString();
-	attachment_list = attachment_list.split(';');
-	for (let i = 0; i < attachment_list.length; i++) {
+async function deleteFile(x) {
+	var {metadata} = await loadMetadata(x);
+	var client = await prepareClient();
+	for (let i = 0; i < metadata.length; i++) {
 		try {
 			console.log(`Deleting chunk ${i}…`);
-			let dat = attachment_list[i].split(',');
-			let channel_id = dat[0], message_id = dat[1];
-			let channel = await dataguild.channels.resolve(channel_id);
+			let channel_id = metadata[i][0], message_id = metadata[i][1];
+			let channel = await client.dataguild.channels.resolve(channel_id);
 			//console.debug(`Found channel ${channel_id}`);
 			let message = await channel.messages.fetch(message_id);
 			//console.debug(`Found message ${message_id}`);
@@ -178,17 +141,24 @@ async function deleteFile(code) {
 		}
 	}
 	console.log(`Done`);
+	client.destroy();
 }
 
-async function getDataGuild(client) {
-	//console.log("Finding data guild…");
-	let dg = client.guilds.cache.find(x => x.name == DATA_GUILD_NAME && x.ownerID == client.user.id);
-	if (!dg) {
-		console.log("Creating new data guild…");
-		dg = await client.guilds.create(DATA_GUILD_NAME, {
-			channels: [{name: META_CHANNEL_NAME}].concat(Array(499).fill({name: DATA_CHANNEL_NAME}))
-		});
-	} else console.log("Found existing data guild");
-	//console.debug(`ID: ${dg.id} Invite: ${(await dg.channels.cache.first().createInvite()).code}`);
-	return dg;
+async function loadMetadata(urlorpath){
+	console.log("Loading metadata file…");
+	if (/^https?:\/\//.test(urlorpath)) {
+		var metadata = (await require("node-fetch")(urlorpath)).body;
+	} else {
+		var metadata = require("fs").readFileSync(urlorpath);
+	}
+	metadata = require("zlib").gunzipSync(metadata).toString();
+	metadata = metadata.split(';');
+	var metametadata = JSON.parse(metadata.shift());
+	metadata = metadata.map(x => x.split(','));
+	metadata.sort((a,b) => {
+		let ai = Number(a[3].split('.').pop());
+		let bi = Number(b[3].split('.').pop());
+		return ai - bi;
+	});
+	return {filename: metametadata.filename, metadata}
 }
